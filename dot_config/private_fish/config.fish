6bfd55fe63fbd57f6cc4369be4747ae2b86cdb2e
Keshav <95571677+Keshav-writes-code@users.noreply.github.com>
2025-03-06 19:48:13 +0530

    Update .config/fish/config.fish Update .config/fish/fish_variables Update .zshrc

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -2,3 +2,5 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 zoxide init fish | source
+
+test -s /home/keshav/.config/nvm/nvm.fish; and source /home/keshav/.config/nvm/nvm.fish
