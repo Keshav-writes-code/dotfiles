8a5459619588f69398c05957dd5cb126f34e9759
Keshav <95571677+Keshav-writes-code@users.noreply.github.com>
2025-08-27 17:38:16 +0530

    Update .config/fish/config.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -6,3 +6,9 @@ end
 zoxide init fish | source
 
 test -s /home/keshav/.config/nvm/nvm.fish; and source /home/keshav/.config/nvm/nvm.fish
+
+alias ls='eza'
+alias l='eza -l --icons --git'
+alias ll='eza -l --icons --git'
+alias la='eza -la --icons --git'
+alias tree='eza --tree --icons'
