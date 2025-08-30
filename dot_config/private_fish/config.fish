2bb9649b590fe80470c904884b7d0bf21d4236a8
Keshav <95571677+Keshav-writes-code@users.noreply.github.com>
2025-08-30 07:22:40 +0530

    Update .config/dconf/user Update .config/fish/config.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -12,3 +12,12 @@ alias l='eza -l --icons --git'
 alias ll='eza -l --icons --git'
 alias la='eza -la --icons --git'
 alias tree='eza --tree --icons'
+
+function y
+    set tmp (mktemp -t "yazi-cwd.XXXXXX")
+    yazi $argv --cwd-file="$tmp"
+    if read -z cwd <"$tmp"; and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+        builtin cd -- "$cwd"
+    end
+    rm -f -- "$tmp"
+end
