ef44e262c24069d119c3975497708ed6da1a095b
keshav-writes-code <dhimankeshav201@gmail.com>
2025-10-01 23:56:01 +0530

    fix: refac for runit

--- dot_config/rofi/executable_powermenu.sh
+++ dot_config/rofi/executable_powermenu.sh
@@ -4,7 +4,7 @@
 ## Github : @adi1090x
 
 # CMDs
-uptime="`uptime -p | sed -e 's/up //g'`"
+uptime="$(uptime -p | sed -e 's/up //g')"
 
 # Options
 shutdown='󰐥'
@@ -15,73 +15,73 @@ logout='󰍃'
 
 # Rofi CMD
 rofi_cmd() {
-	rofi -dmenu \
-		-p "" \
-		-mesg "Uptime: $uptime" \
-		-theme "$HOME/.config/rofi/themes/powermenu.rasi"
+  rofi -dmenu \
+    -p "" \
+    -mesg "Uptime: $uptime" \
+    -theme "$HOME/.config/rofi/themes/powermenu.rasi"
 }
 
 # Pass variables to rofi dmenu
 run_rofi() {
-	echo -e "$lock\n$suspend\n$logout\n$reboot\n$shutdown" | rofi_cmd
+  echo -e "$lock\n$suspend\n$logout\n$reboot\n$shutdown" | rofi_cmd
 }
 
 # Execute Command
 run_cmd() {
-	case $1 in
-		--shutdown)
-			systemctl poweroff
-			;;
-		--reboot)
-			systemctl reboot
-			;;
-		--suspend)
-			mpc -q pause
-			amixer set Master mute
-			systemctl suspend
-			;;
-		--logout)
-			case "$DESKTOP_SESSION" in
-				openbox)
-					openbox --exit
-					;;
-				bspwm)
-					bspc quit
-					;;
-				dwm)
-					pkill dwm
-					;;
-				i3)
-					i3-msg exit
-					;;
-				plasma)
-					qdbus org.kde.ksmserver /KSMServer logout 0 0 0
-					;;
-			esac
-			;;
-	esac
+  case $1 in
+  --shutdown)
+    sudo poweroff
+    ;;
+  --reboot)
+    sudo reboot
+    ;;
+  --suspend)
+    mpc -q pause
+    amixer set Master mute
+    sudo zzz # antiX suspend command (from uswsusp)
+    ;;
+  --logout)
+    case "$DESKTOP_SESSION" in
+    openbox)
+      openbox --exit
+      ;;
+    bspwm)
+      bspc quit
+      ;;
+    dwm)
+      pkill dwm
+      ;;
+    i3)
+      i3-msg exit
+      ;;
+    plasma)
+      qdbus org.kde.ksmserver /KSMServer logout 0 0 0
+      ;;
+    esac
+    ;;
+  esac
 }
 
 # Actions
 chosen="$(run_rofi)"
 case "${chosen}" in
-    "${shutdown}")
-		run_cmd --shutdown
-        ;;
-    "${reboot}")
-		run_cmd --reboot
-        ;;
-    "${lock}")
-		if [[ -x '/usr/bin/betterlockscreen' ]]; then
-			betterlockscreen -l
-		elif [[ -x '/usr/bin/i3lock' ]]; then
-			i3lock
-		fi
-        ;;
-    "${suspend}")
-		run_cmd --suspend
-        ;;
-    "${logout}")
-		run_cmd --logout
-        ;;
+"${shutdown}")
+  run_cmd --shutdown
+  ;;
+"${reboot}")
+  run_cmd --reboot
+  ;;
+"${lock}")
+  if [[ -x '/usr/bin/betterlockscreen' ]]; then
+    betterlockscreen -l
+  elif [[ -x '/usr/bin/i3lock' ]]; then
+    i3lock
+  fi
+  ;;
+"${suspend}")
+  run_cmd --suspend
+  ;;
+"${logout}")
+  run_cmd --logout
+  ;;
 esac
