5dbfaa816bf1e48420cfb2a72cd6aa6e467ba86a
keshav-writes-code <dhimankeshav201@gmail.com>
2026-04-04 11:59:45 +0000

    fix: make the first wallpaper to be shown by the aww -img command. also increse transition duration

--- dot_config/niri/executable_awww_randomize.sh
+++ dot_config/niri/executable_awww_randomize.sh
@@ -3,15 +3,15 @@
 # at a set interval.
 
 DEFAULT_INTERVAL=300 # In seconds
-RESIZE_TYPE="crop"
 export AWWW_TRANSITION_FPS="${AWWW_TRANSITION_FPS:-60}"
 export AWWW_TRANSITION_STEP="${AWWW_TRANSITION_STEP:-2}"
 export AWWW_TRANSITION="${AWWW_TRANSITION:-random}"
-export AWWW_TRANSITION_DURATION="${AWWW_TRANSITION_DURATION:-10}"
+export AWWW_TRANSITION_DURATION="${AWWW_TRANSITION_DURATION:-20}"
 
+sleep 0.2
 while true; do
   # Pick one random file and set it
   img=$(find ~/Pictures/Wallpapers/ -type f | shuf -n 1)
-  awww img --resize="$RESIZE_TYPE" "$img"
+  awww img --resize=crop "$img"
   sleep "${2:-$DEFAULT_INTERVAL}"
 done
