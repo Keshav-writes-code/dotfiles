abf97c075f9ec6735804d5120a83410f35bd065d
keshav-writes-code <dhimankeshav201@gmail.com>
2026-05-13 03:43:18 +0530

    refac: remove legacy system setup

--- user_shell_scripts/executable_backup_master_drive.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-rclone sync \
-  "/media/Master_Drive/" \
-  "gdrive:Computer Storage Backup/Master Drive" \
-  --exclude "/\$RECYCLE.BIN/**" \
-  --exclude "(Useless Files)/**" \
-  -P --delete-excluded
